314fa0a5c3c2745a9333fbbc3cebcb51983f9785
MengZeLee <adnt587@gmail.com>
2019-01-11 15:54:05 +0800

    add exchange create function

--- main.go
+++ main.go
@@ -3,8 +3,18 @@ package main
 import (
 	"math/rand"
 	"time"
+
+	"github.com/go-rest/pkg/exchange"
+	"github.com/go-rest/pkg/server"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
+	resource := server.NewResource("/")
+	exchange := exchange.NewExChanger()
+	resource.AddMethod(server.Post, "", exchange.CreateExChange)
+
+	apiServerHandler := server.NewAPIServerHandler()
+	apiServerHandler.RegisterResource(resource)
+	apiServerHandler.ServeHTTP()
 }
