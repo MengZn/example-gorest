d5d8e9e11c0c70ef1f58ae47046c999ce1985151
MengZeLee <adnt587@gmail.com>
2019-01-16 11:37:42 +0800

    fix http: multiple response.WriteHeader calls

--- pkg/exchange/exchange.go
+++ pkg/exchange/exchange.go
@@ -2,7 +2,6 @@ package exchange
 
 import (
 	"fmt"
-	"net/http"
 
 	"example-gorest/pkg/utils"
 
@@ -43,10 +42,8 @@ func (ex *ExChanger) CreateExChange(request *restful.Request, response *restful.
 	case resp := <-responseChan:
 		if resp.Error != "" {
 			fmt.Printf("Create %s:%s is ERROR %s!!\n", dollar.Name, dollar.Value, resp.Error)
-			response.WriteHeader(http.StatusOK)
 		} else {
 			fmt.Printf("Create %s:%s success\n", dollar.Name, dollar.Value)
-			response.WriteHeader(http.StatusCreated)
 		}
 		response.WriteEntity(resp)
 	}
@@ -65,10 +62,8 @@ func (ex *ExChanger) DelExChange(request *restful.Request, response *restful.Res
 	case resp := <-responseChan:
 		if resp.Error != "" {
 			fmt.Printf("Delete %s ERROR %s!!\n", dollar.Name, resp.Error)
-			response.WriteHeader(http.StatusOK)
 		} else {
 			fmt.Printf("Delete %s success\n", dollar.Name)
-			response.WriteHeader(http.StatusOK)
 		}
 		response.WriteEntity(resp)
 	}
@@ -87,10 +82,8 @@ func (ex *ExChanger) GetExChange(request *restful.Request, response *restful.Res
 	case resp := <-responseChan:
 		if resp.Error != "" {
 			fmt.Printf("Get %s ERROR %s!!\n", dollar.Name, resp.Error)
-			response.WriteHeader(http.StatusOK)
 		} else {
 			fmt.Printf("Get %s success value is %s \n", dollar.Name, dollar.Value)
-			response.WriteHeader(http.StatusOK)
 		}
 		response.WriteEntity(resp)
 	}
@@ -111,10 +104,8 @@ func (ex *ExChanger) EditExChange(request *restful.Request, response *restful.Re
 	case resp := <-responseChan:
 		if resp.Error != "" {
 			fmt.Printf("Get %s ERROR %s!!\n", dollar.Name, resp.Error)
-			response.WriteHeader(http.StatusOK)
 		} else {
 			fmt.Printf("Edit %s success value is %s \n", dollar.Name, dollar.Value)
-			response.WriteHeader(http.StatusOK)
 		}
 		response.WriteEntity(resp)
 	}
